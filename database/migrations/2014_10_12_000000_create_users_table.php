04d572062e94359ed519b2401a654afdd6780291
Igor Gligorevic <igor.gligorevic@hotmail.com>
2020-11-05 14:14:30 +0100

    Product and user seeders created, product factory created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('country');
+            $table->string('company');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+//            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+//            $table->rememberToken();
             $table->timestamps();
         });
     }
